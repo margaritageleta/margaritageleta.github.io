5eb73dddea302906f4ed715aaa342f8be3413bda
margaritageleta <noticiasmundiales99@gmail.com>
2020-08-21 14:08:02 +0200

    change scroll params

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 var sdegree = 0;
 
 $(window).scroll(function() {
-    var height = "" + (20 - window.scrollY/100) + "vh";
+    var height = "" + (10 - window.scrollY/90) + "vh";
     $("#border").css({"height" : height});
 });
