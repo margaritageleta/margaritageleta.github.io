40fa977d6d3e022ab058b5cd5902ac0dc353085c
Margarita Geleta <noticiasmundiales99@gmail.com>
2022-10-13 22:25:12 -0700

    Change the styling of the website

--- js/loadTags.js
+++ js/loadTags.js
@@ -1,23 +1,23 @@
 function loadMetaTags() {
     var metas = `
     <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=3">
+	<meta name="viewport" content="width=device-width, initial-scale=0.5 minimum-scale=1, maximum-scale=3">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<meta name="author" content="3omni">
 	<meta name="web_author" content="3omni">
 	<meta name="description" content="Rita Geleta's website">
 	<meta name="keywords" content="Margarita Rita Geleta 3omni">
 	<meta name="revisit-after" content="7 days">
-	<meta name="reply-to" content="rita.geleta@jediupc.com">
+	<meta name="reply-to" content="geleta@berkeley.edu">
 	<meta name="HandheldFriendly" content="true">
 	<meta name="MobileOptimized" content="320">
 	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="city" content="Barcelona">
-	<meta name="country" content="ES">
+	<meta name="city" content="San Francisco">
+	<meta name="country" content="US">
 	<meta name="distribution" content="global">
 	<meta http-equiv="content-language" content="en">
 
-	<meta property="og:title" content="3omni @ your device">
+	<meta property="og:title" content="3omni">
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="3omni">
 	<meta property="og:description" content="Rita Geleta's website">
@@ -29,12 +29,12 @@ function loadMetaTags() {
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:site" content="@ritageleta">
 	<meta name="twitter:creator" content="@ritageleta">
-	<meta name="twitter:title" content="3omni @ your device">
+	<meta name="twitter:title" content="3omni">
 	<meta name="twitter:description" content="Rita Geleta's website">
 	<meta property="twitter:image" content="./img/ini.png">
 	<meta property="twitter:image:secure_url" content="./img/ini.png">
 
-	<meta name="apple-mobile-web-app-title" content="3omni @ your devicep">
+	<meta name="apple-mobile-web-app-title" content="3omni">
 	<meta name="application-name" content="3omni">
 	<meta name="theme-color" content="hotpink"> 
     `;
